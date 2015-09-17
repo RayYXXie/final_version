7ff214b129c77716ad331879e54d6313233eb01a
RingYXXie <407691676@qq.com>
2015-09-17 15:44:27 +0800

    Update killTask.sh

--- cargosample-parent/killTask.sh
+++ cargosample-parent/killTask.sh
@@ -2,6 +2,7 @@ b=`ps -ef | grep tomcat | grep -v 'tomcat-jenkins' | grep -v 'grep'|wc -l`
 if [ $b \> 0  ]
 then
         kill -9 `ps -ef | grep tomcat | grep -v 'tomcat-jenkins' | grep -v 'grep'|awk '{print $2}'`
+        rm -rf /usr/ffg_server/apache-tomcat-6.0.44-2/webapps/SocketClient
 else
         echo "No tomcats need to be killed"
 fi
