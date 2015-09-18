29c005812846d7a832ac34f4b8553d1fd5438eb4
RingYXXie <407691676@qq.com>
2015-09-18 09:56:48 +0800

    Create killSTServer.sh

---
+++ cargosample-parent/killSTServer.sh
@@ -0,0 +1,17 @@
+b=`ps -ef | grep tomcat | grep -v 'tomcat-jenkins' | grep -v 'grep'|wc -l`
+if [ $b \> 0  ]
+then
+        kill -9 `ps -ef | grep tomcat | grep -v 'tomcat-jenkins' | grep -v 'grep'|awk '{print $2}'`
+        rm -rf /usr/ffg_server/apache-tomcat-6.0.44/webapps/STsocketClient
+else
+        echo "No tomcats need to be killed"
+fi
+
+
+b1=`ps -ef | grep 'startcontrol.STsocketServer' | grep -v 'grep'|wc -l`
+if [ $b1 \> 0  ]
+then
+        kill -9 `ps -ef | grep 'startcontrol.STsocketServer' | grep -v 'grep'|awk '{print $2}'`
+else
+        echo "No SockectServers need to be killed"
+fi
