5884b7049bc0d600fe7494f05d139264408ce03d
RingYXXie <407691676@qq.com>
2015-09-18 17:51:15 +0800

    Update HandlerServlet.java

--- cargosample-parent/SocketClient/src/main/java/servlet/HandlerServlet.java
+++ cargosample-parent/SocketClient/src/main/java/servlet/HandlerServlet.java
@@ -17,7 +17,7 @@ public class HandlerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	static int viewCount = 0;
    	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		final String id = request.getParameter("id");
 		viewCount++;
 		System.out.println(" -----------------------------------viewCount-----------------------------------     "+viewCount);
