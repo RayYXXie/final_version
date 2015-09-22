4281c7e8bff9f82fb0e9e1cc28d6523ea3d330d0
RingYXXie <407691676@qq.com>
2015-09-22 16:24:05 +0800

    Update AppTest.java

--- cargosample-parent/entry/src/test/java/org/entry/AppTest.java
+++ cargosample-parent/entry/src/test/java/org/entry/AppTest.java
@@ -8,5 +8,16 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest{
-
+    @Test
+      public void test1(){
+      	System.out.println("this is test1");
+      	String str1 = "abc";
+      	String str2 = "bcd";
+      	if(!str1.equals(str2)){
+      		System.out.println("str1 不等于str2");
+      	}else{
+      		System.out.println("str1 等于str2");
+      	}
+      	
+      }
 }
