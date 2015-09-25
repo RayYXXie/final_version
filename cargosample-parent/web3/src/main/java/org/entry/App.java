cb6df55db4db034e4012264a7ab17d1009ed4970
Admin <Admin@192.168.4.41>
2015-09-25 10:46:54 +0800

    add some code to web1-web5/src/main/java

--- cargosample-parent/web3/src/main/java/org/entry/App.java
+++ cargosample-parent/web3/src/main/java/org/entry/App.java
@@ -6,8 +6,13 @@ package org.entry;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public void add(int a,int b){
+    	if(a == b){
+    		System.out.println("a=b");
+    	}else if(a>b){
+    		System.out.println("a>b");
+    	}else{
+    		System.out.println("a<b");
+    	}
     }
 }
