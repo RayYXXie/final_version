8cad1e38dcde9e24ddc264a553adc917be70aa8c
RingYXXie <407691676@qq.com>
2015-09-29 19:47:22 +0800

    Create AppTest.java

---
+++ cargosample-parent/SocketServer/src/test/java/ffg/AppTest.java
@@ -0,0 +1,17 @@
+package ffg;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		
+		int a = 1;
+		int b = 2;
+		if(a<b){
+			System.out.println("aaa");
+		}
+	}
+
+}
